9d3b2b7bfc71da963064130ef9b42bb7ef2535ed
ntsk <ntsk@ntsk.jp>
2025-11-22 01:37:31 +0900

    Fix duplicate naming and typos in binding files

--- Package.swift
+++ Package.swift
@@ -9,16 +9,16 @@ if FileManager.default.fileExists(atPath: "src/scanner.c") {
 }
 
 let package = Package(
-    name: "TreeSitterTreeSitterVcl",
+    name: "TreeSitterVcl",
     products: [
-        .library(name: "TreeSitterTreeSitterVcl", targets: ["TreeSitterTreeSitterVcl"]),
+        .library(name: "TreeSitterVcl", targets: ["TreeSitterVcl"]),
     ],
     dependencies: [
         .package(name: "SwiftTreeSitter", url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.9.0"),
     ],
     targets: [
         .target(
-            name: "TreeSitterTreeSitterVcl",
+            name: "TreeSitterVcl",
             dependencies: [],
             path: ".",
             sources: sources,
@@ -29,12 +29,12 @@ let package = Package(
             cSettings: [.headerSearchPath("src")]
         ),
         .testTarget(
-            name: "TreeSitterTreeSitterVclTests",
+            name: "TreeSitterVclTests",
             dependencies: [
                 "SwiftTreeSitter",
-                "TreeSitterTreeSitterVcl",
+                "TreeSitterVcl",
             ],
-            path: "bindings/swift/TreeSitterTreeSitterVclTests"
+            path: "bindings/swift/TreeSitterVclTests"
         )
     ],
     cLanguageStandard: .c11
